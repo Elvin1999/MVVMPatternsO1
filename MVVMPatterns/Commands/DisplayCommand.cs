ccc8717fa3313e799b92bfc433d91d32a2910da2
e.camalzade <e.camalzade@STHQ0127-03>
2022-09-02 14:54:34 +0400

    Add project files.

---
+++ MVVMPatterns/Commands/DisplayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMPatterns.Commands
+{
+    public class DisplayCommand:ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public Action _action;
+
+        public DisplayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            _action.Invoke();
+        }
+    }
+}
---
+++ MVVMPatterns/Views/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using MVVMPatterns.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMPatterns
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // public string Text { get; set; } = "Salam";
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    this.DataContext = this;
+        //}
+
+        //    private void Help_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        //    {
+        //        if (Text.Length > 10)
+        //        {
+        //            e.CanExecute = true;
+        //        }
+        //        else
+        //        {
+        //            e.CanExecute = false;
+        //        }
+
+        //    }
+
+        //    private void Help_Executed(object sender, ExecutedRoutedEventArgs e)
+        //    {
+        //        Process.Start("msedge.exe");
+        //    }
+
+
+
+        //public string Text { get; set; } = "Salam";
+
+
+
+        public SendCommand SendCommand { get; set; }
+        public DisplayCommand DisplayCommand { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            SendCommand = new SendCommand(Send);
+            DisplayCommand = new DisplayCommand(Display);
+
+        }
+
+        private void Display()
+        {
+            MessageBox.Show("Message was sent successfully");
+        }
+
+        private void Send()
+        {
+            MessageBox.Show("Message was sent successfully");
+        }
+    }
+}
